9c286bc11b900ada9586d315d15042f575268589
SAUL HERNANDEZ <saul7.hernandez@outlook.com>
2018-02-01 13:40:49 -0400

    Init Internationalization

---
+++ ForeignExchange/ForeignExchange/ForeignExchange/Helpers/Languajes.cs
@@ -0,0 +1,36 @@
+﻿using ForeignExchange.Interfaces;
+using ForeignExchange.Resources;
+using Xamarin.Forms;
+
+namespace ForeignExchange.Helpers
+{
+    public static class Languajes
+    {
+        static Languajes()
+        {
+            var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Accept
+        {
+            get { return Resource.Accept; }
+        }
+
+        public static string AmountValidation
+        {
+            get { return Resource.AmountValidation; }
+        }
+
+        public static string Error
+        {
+            get { return Resource.Error; }
+        }
+
+        public static string Title
+        {
+            get { return Resource.Title; }
+        }
+    }
+}
